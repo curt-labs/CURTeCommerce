95642e1084b55f8e2f16bda52cd5ad6fa5ebf5e4
Jessica Janiuk <jessica.janiuk@gmail.com>
2012-09-14 11:44:57 -0600

    Fixed EDI PO generation

--- PlatformWebRole/MvcWebRole1/Models/EDI.cs
+++ PlatformWebRole/MvcWebRole1/Models/EDI.cs
@@ -25,8 +25,8 @@ namespace EcommercePlatform.Models {
                         int linecount = 1;
                         // linecount is just for the PO section and doesn't include the head or tail
                         // next two lines are head
-                        edicontent += "ISA*00*          *00*          *12*" + settings.Get("EDIPhone") + "     *01*809988975      *" + String.Format("{0:yyMMdd}*{0:hhmm}", DateTime.Now) + "*U*00401*" + ponumber + "*0*P*>~" + Environment.NewLine;
-                        edicontent += "GS*PO*" + settings.Get("EDIPhone") + "*809988975*" + String.Format("{0:yyyyMMdd}*{0:hhmm}", DateTime.Now) + "*" + ponumber + "*X*004010~" + Environment.NewLine;
+                        edicontent += "ISA*00*          *00*          *12*" + settings.Get("EDIPhone") + "     *01*809988975      *" + String.Format("{0:yyMMdd}*{0:hhmm}", DateTime.Now) + "*U*00401*" + order.payment_id.ToString("000000000") + "*0*P*>~" + Environment.NewLine;
+                        edicontent += "GS*PO*" + settings.Get("EDIPhone") + "*809988975*" + String.Format("{0:yyyyMMdd}*{0:hhmm}", DateTime.Now) + "*" + order.payment_id.ToString("000000000") + "*X*004010~" + Environment.NewLine;
                         // begin PO section
                         edicontent += "ST*850*" + order.payment_id + "~" + Environment.NewLine;
                         linecount++;
@@ -34,7 +34,7 @@ namespace EcommercePlatform.Models {
                         linecount++;
                         edicontent += "CUR*BT*USD~" + Environment.NewLine;
                         linecount++;
-                        edicontent += "REF*CO*" + order.payment_id + "~" + Environment.NewLine;
+                        edicontent += "REF*CO*" + ponumber + "~" + Environment.NewLine;
                         linecount++;
                         edicontent += "REF*IA*" + settings.Get("CURTAccount") + "~" + Environment.NewLine;
                         linecount++;
@@ -67,8 +67,8 @@ namespace EcommercePlatform.Models {
                         edicontent += "SE*" + linecount + "*" + order.payment_id + "~" + Environment.NewLine;
                         // end PO section
                         // begin Tail section
-                        edicontent += "GE*1*" + ponumber + "~" + Environment.NewLine;
-                        edicontent += "IEA*1*" + ponumber + "~";
+                        edicontent += "GE*1*" + order.payment_id.ToString("000000000") + "~" + Environment.NewLine;
+                        edicontent += "IEA*1*" + order.payment_id.ToString("000000000") + "~";
 
                         // write file
                         DiscountBlobContainer blobcontainer = BlobManagement.GetContainer("edi");
