f336e3d4c7095c75bac26dde0b67fd89bef4384e
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-06-17 15:57:33 -0600

    Added parent categories call to infocontroller and fixed breadcrumbs in category view

--- PlatformWebRole/MvcWebRole1/Controllers/InfoController.cs
+++ PlatformWebRole/MvcWebRole1/Controllers/InfoController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using EcommercePlatform.Models;
 
 namespace EcommercePlatform.Controllers {
     public class InfoController : BaseController {
 
-        public ActionResult Index() {
+        public async Task<ActionResult> Index() {
+            var pcats = CURTAPI.GetParentCategoriesAsync();
+            await Task.WhenAll(new Task[] { pcats });
+            ViewBag.parent_cats = await pcats;
             return View();
         }
 
