2d1b7a08f8c183c5584fa2ce93d00d368d9c72a6
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-06-20 11:27:25 -0600

    Added payment.js to prevent double submissions to the payment system

---
+++ PlatformWebRole/MvcWebRole1/Scripts/custom/payment.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $(document).on('submit', '#ccform', function () {
+        $('#paymentSubmit').val('Submitting...');
+        $('#paymentSubmit').attr('disabled', 'disabled');
+    });
+});
